6e80f6a73b08932cf7352a2a9fd5ff5898bb982a
ThisIsMyPlace <thisismyplace@hushmail.me>
2014-03-29 11:39:34 +0000

    Toast to reboot

--- src/org/thisismyplace/magicpasswd/MainActivity.java
+++ src/org/thisismyplace/magicpasswd/MainActivity.java
@@ -1,12 +1,33 @@
 package org.thisismyplace.magicpasswd;
 
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.widget.Toast;
 
-public class MainActivity extends PreferenceActivity {
+public class MainActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.settings);
     }
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+	}
+
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+		if(key.equals("pref_enabled")) {
+			Toast.makeText(this, R.string.reboot_warning, Toast.LENGTH_SHORT).show();
+		}
+	}
 }
